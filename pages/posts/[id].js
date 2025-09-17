2f5e8e08e61d9fb3ee6b0111c81be33064f50a8d
linc0306 <chienhung.lin@usach.cl>
2025-09-17 10:55:08 -0300

    Aplicar date-fns y style de css en la fecha de los posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,9 @@
-import Layout from '../../components/layout';
 import Head from 'next/head';
+
+import Date from '../../components/date';
+import Layout from '../../components/layout';
+
+import utilStyles from '../../styles/utils.module.css';
 
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
@@ -28,13 +32,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
